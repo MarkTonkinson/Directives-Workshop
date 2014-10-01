f1f5a1ad29cb4069dd643f904def4e5071e1c802
Mark Tonkinson <DHTMaster@gmail.com>
2014-10-01 06:54:21 -0600

    forgot to save

--- mainService.js
+++ mainService.js
@@ -1,10 +1,12 @@
 var app = angular.module('directiveWorkshop');
 
-app.service('mainService', function($http){
+app.service('mainService', function($http, $q){
 	this.getData = function(artist) {
+		var deferred = $q.defer();
         return $http({'method': 'JSONP', 'url': 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK' })
             .then(function (data) {
-                return data;
+                deferred.resolve(data);
             });
+            return deferred.promise
     };
 })
